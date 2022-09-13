bc9536ac7e5cdf6f48482cfe31b8f48afbf43d4c
Satyam Singh <officialsatyam1010@gmail.com>
2022-09-13 16:04:25 +0530

    Refactored
    - code refactoring
    - made change in xpath
    - added delay function to wait so that page can be loaded fully
    - Added quit logic if job scrapped

--- linkdin.js
+++ linkdin.js
@@ -8,32 +8,47 @@ let driver = new Builder().forBrowser('chrome').build()
 // Here it is opening the chrome page.
 driver.get('https://www.linkedin.com/home/?originalSubdomain=in/')
 
-let scrollAndClickSeeMore = async function () {
+// wait function;
+const delay = ms => new Promise(res => setTimeout(res, ms));
+let count = 0;
+let scrollAndClickSeeMore = async function (myInterval) {
 
     try {
+
+
         // scroll up to the this element
-        let scrollUpTo = await driver.findElement(By.xpath("/html/body/main/div/section/button"))
-        
+        scrollUpTo = await driver.findElement(By.xpath("/html/body/div[1]/footer")) // /html/body/main/div/section/button
+
         // this will scroll the page scrollUpTo element
         await driver.executeScript("arguments[0].scrollIntoView(true)", scrollUpTo);
-        
+
         // Element having list of jobs here
-        let listOfJob = await  driver.findElement(By.xpath("/html/body/main/div/section/ul"))
+        let listOfJob = await driver.findElement(By.xpath(`//*[@id="main-content"]/section[2]/ul`))
 
         // Loading HTML data from the listOfJob Element
         let htmlOfJobs = await listOfJob.getAttribute("outerHTML");
-        
+
         // scraping full Detail of company profile with jobTitle, jobDescription etc.....
-        let companyDetails = await scrap.jobDetails(htmlOfJobs)
-        console.log(companyDetails)
+        let companyDetails = await scrap.jobDetails(htmlOfJobs);
+        if (companyDetails.length !== count) {
+            console.log(companyDetails);
+            count = companyDetails.length;
+        } else {
+            console.log("All Scrapped Successfully!");
+            clearInterval(myInterval);
+            driver.quit()
+        }
+        await delay(500);
 
         // click on the element as soon as seen to the webpage
-        await scrollUpTo.click();
+        // await scrollUpTo.click();
+
 
         // returning true as promise is resolve
         return true
     }
     catch (err) {
+        console.error('Error in function:scrollAndClickSeeMore', err);
         //returning false as promise is not resolve(reject)
         return false;
     }
@@ -42,35 +57,42 @@ let scrollAndClickSeeMore = async function () {
 
 let searchjobByLocation = async function () {
 
-    // jobPosition button 
-    let jobPosition = await driver.findElement(By.xpath('/html/body/main/section[1]/section/div[1]/button'))
-    // click to change button as a input box
-    jobPosition.click()
+    try {
+        // jobPosition button 
+        let jobPosition = await driver.findElement(By.xpath(`//*[@id="main-content"]/section[8]/div/div/div[2]/ul/li[2]/a`));
+        // click to change button as a input box
+        jobPosition.click()
+        await delay(5000);
+        // jobPositon is now input box to enter the job location
+        jobPosition = await driver.findElement(By.xpath(`//*[@id="JOBS"]/section[1]/input`))
+        // Input for the jobTitle and location preference
+        let jobTitle = input.question('Enter the jobTitle you want to search: ')
+        let location = input.question('Enter your location preference: ')
 
-    // jobPositon is now input box to enter the job location
-    jobPosition = await driver.findElement(By.xpath('//*[@id="JOBS"]/section[1]/input'))
-    // Input for the jobTitle and location preference
-    let jobTitle = input.question('Enter the jobTitle you want to search: ')
-    let location = input.question('Enter your location preference: ')
+        //sending keys to the browser
+        jobPosition.sendKeys(jobTitle)
 
-    //sending keys to the browser
-    jobPosition.sendKeys(jobTitle)
+        await delay(5000);
+        // location of the jobPositon
+        let jobLocation = await driver.findElement(By.xpath(`//*[@id="JOBS"]/section[2]/input`))
+        // bydefault there is new Delhi clearing it
+        jobLocation.clear()
 
-    // location of the jobPositon
-    let jobLocation = await driver.findElement(By.xpath('//*[@id="JOBS"]/section[2]/input'))
-    // bydefault there is new Delhi clearing it
-    jobLocation.clear()
+        await delay(5000);
 
-    //sending the lcation to the browser and clicking enter to search the desired jobs on prefered location
-    await jobLocation.sendKeys(location, Key.ENTER)
+        //sending the lcation to the browser and clicking enter to search the desired jobs on prefered location
+        await jobLocation.sendKeys(location, Key.ENTER)
 
-    // In each 3 seconds calling the function again and again, function is working synchronously
-    let myInterval = setInterval(async () => {
-        // Waiting for the server response
-        let booleans = await scrollAndClickSeeMore()
-        // if booleans is false will stop our myInterval
-        if (!booleans) clearInterval(myInterval);
-    }, 3000)
+        // In each 3 seconds calling the function again and again, function is working synchronously
+        let myInterval = setInterval(async () => {
+            // Waiting for the server response
+            let booleans = await scrollAndClickSeeMore(myInterval)
+            // if booleans is false will stop our myInterval
+            if (!booleans) clearInterval(myInterval);
+        }, 3000)
+    } catch (error) {
+        console.error('Error in function:searchjobByLocation', error); s
+    }
 
 }
 
--- scrapingjobs.js
+++ scrapingjobs.js
@@ -4,45 +4,49 @@ module.exports = {
 
     jobDetails: async (htmlData) => {
 
-        // Loading web page source into cheerio instance.
-        const cheerioInstance = await cheerio.load(htmlData)
+        try {
+            // Loading web page source into cheerio instance.
+            const cheerioInstance = await cheerio.load(htmlData)
 
-        // Declaing an array where gonna store full companyDetails.
-        let companyDetails = []
+            // Declaing an array where gonna store full companyDetails.
+            let companyDetails = []
 
-        // Looping over all the li containing full Details of company.
-        await cheerioInstance('ul.jobs-search__results-list li')
-            .map((index, element) => {
+            // Looping over all the li containing full Details of company.
+            await cheerioInstance('ul.jobs-search__results-list li')
+                .map((index, element) => {
 
-                // jobTitle under the li inside a tag.
-                let jobTitle = cheerioInstance(element).find('a.result-card__full-card-link span').text()
-                // link of the main page of jobDetails.
-                let link = cheerioInstance(element).find('a.result-card__full-card-link').attr('href')
-                // imageLink extracting from the image tag.
-                let imageLink = cheerioInstance(element).find('img').attr('src')
-                // extracting company name from inside the a tag of h4 tag.
-                let companyName = cheerioInstance(element).find('div > h4 > a').text()
-                // extracted Location where company locate.
-                let companyLocation = cheerioInstance(element).find('div > div > span').text()
-                // Desciption of the post company recruiting.
-                let postDescription = cheerioInstance(element).find('div > div > p').text()
-                // The time of the post is posted on linkdin by company. 
-                let postingTime = cheerioInstance(element).find('div > div > time').text()
+                    // jobTitle under the li inside a tag.
+                    let jobTitle = cheerioInstance(element).find('a.base-card__full-link span').text()
+                    // link of the main page of jobDetails.
+                    let link = cheerioInstance(element).find('a.base-card__full-link').attr('href')
+                    // imageLink extracting from the image tag.
+                    let imageLink = cheerioInstance(element).find('img').attr('src')
+                    // extracting company name from inside the a tag of h4 tag.
+                    let companyName = cheerioInstance(element).find('div > h4 > a').text()
+                    // extracted Location where company locate.
+                    let companyLocation = cheerioInstance(element).find('div > div > span').text()
+                    // Desciption of the post company recruiting.
+                    // let postDescription = cheerioInstance(element).find('div > div > p').text()
+                    // The time of the post is posted on linkdin by company. 
+                    let postingTime = cheerioInstance(element).find('div > div > time').text()
 
-                // storing the details in an array
-                companyDetails.push({
-                    "jobTitle": jobTitle,
-                    "link": link,
-                    "imageLink": imageLink,
-                    "companyName": companyName,
-                    "companyLocation": companyLocation,
-                    "postDescription": postDescription,
-                    "postingTime": postingTime
+                    // storing the details in an array
+                    companyDetails.push({
+                        "jobTitle": jobTitle ? jobTitle.replace(/(\r\n|\n|\r)/gm, "").trim() : '',
+                        "link": link ? link.replace(/(\r\n|\n|\r)/gm, "").trim() : '',
+                        "imageLink": imageLink ? imageLink.replace(/(\r\n|\n|\r)/gm, "").trim() : '',
+                        "companyName": companyName ? companyName.replace(/(\r\n|\n|\r)/gm, "").trim() : '',
+                        "companyLocation": companyLocation ? companyLocation.replace(/(\r\n|\n|\r)/gm, "").trim() : '',
+                        // "postDescription": postDescription,
+                        "postingTime": postingTime ? postingTime.replace(/(\r\n|\n|\r)/gm, "").trim() : ''
+                    })
+
                 })
 
-            })
-
-        return companyDetails;
+            return companyDetails;
+        } catch (error) {
+            console.error(`Error While Scraping Company Details`, error);
+        }
 
     }
 
